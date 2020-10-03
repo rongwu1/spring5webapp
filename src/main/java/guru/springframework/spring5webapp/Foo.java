71f28ecde21e97827ff646154c1712183661f738
Neal Yin <nianlongy@yahoo.com>
2020-10-02 19:38:28 -0700

    First file to test

---
+++ src/main/java/guru/springframework/spring5webapp/Foo.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Foo {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
